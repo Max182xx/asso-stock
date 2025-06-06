1f7cea9bfee4794889c438907e80bc2d067ad3be
Max182xx <m.garciapro34@gmail.com>
2025-06-06 18:05:54 +0200

    Finalisations des catégories (sup et mod)

--- app/category/page.tsx
+++ app/category/page.tsx
@@ -1,55 +1,97 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Wrapper from "../components/Wrapper";
 import CategoryModal from "../components/CategoryModal";
-import { createCategory, updateCategory } from "../actions";
+import { createCategory, deleteCategory, readCategories, updateCategory } from "../actions";
 import { toast } from "react-toastify";
-import { useUser } from '@clerk/nextjs'
+import { useUser } from "@clerk/nextjs";
+import { Category } from "@prisma/client";
+import EmptyState from "../components/EmptyState";
+import { Category } from "@prisma/client";
+import { Pencil, Trash } from "lucide-react";
 
 const page = () => {
-  const { user } = useUser()
-  const email = user?.primaryEmailAddress?.emailAddress as string
+  const { user } = useUser();
+  const email = user?.primaryEmailAddress?.emailAddress as string;
 
-  const [name, setName] = useState("")
-  const [description, setDescription] = useState("")
-  const [loading, setLoading] = useState(false)
-  const [editMode, setEditMode] = useState(false)
-  const [editingCategoryId, setEditingCategoryId] = useState<string | null>(null)
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [editMode, setEditMode] = useState(false);
+  const [editingCategoryId, setEditingCategoryId] = useState<string | null>(
+    null
+  );
+  const [categories, setCategories] = useState<Category[]>([]);
 
-   const openCreateModal = () => {
+  const loadCategories = async () => {
+    if (email) {
+      const data = await readCategories(email);
+      if (data) setCategories(data);
+    }
+  };
+
+  useEffect(() => {
+    loadCategories();
+  }, [email]);
+
+  const openCreateModal = () => {
     setName("");
     setDescription("");
     setEditMode(false);
-    (document.getElementById("category_modal") as HTMLDialogElement)?.showModal()
-  }
+    (
+      document.getElementById("category_modal") as HTMLDialogElement
+    )?.showModal();
+  };
 
   const closeModal = () => {
     setName("");
     setDescription("");
     setEditMode(false);
-    (document.getElementById("category_modal") as HTMLDialogElement)?.close()
-  }
+    (document.getElementById("category_modal") as HTMLDialogElement)?.close();
+  };
 
   const handleCreateCategory = async () => {
-    setLoading(true)
+    setLoading(true);
     if (email) {
-      await createCategory(name, email, description)
+      await createCategory(name, email, description);
     }
-    closeModal()
-    setLoading(false)
-    toast.success("Catégorie créée avec succès.")
-  }
+    await loadCategories();
+    closeModal();
+    setLoading(false);
+    toast.success("Catégorie créée avec succès.");
+  };
 
   const handleUpdateCategory = async () => {
-    if (!editingCategoryId) return
-    setLoading(true)
+    if (!editingCategoryId) return;
+    setLoading(true);
     if (email) {
-      await updateCategory(editingCategoryId, email, name, description)
+      await updateCategory(editingCategoryId, email, name, description);
     }
-    closeModal()
-    setLoading(false)
-    toast.success("Catégorie mise à jour avec succès.")
+    await loadCategories();
+    closeModal();
+    setLoading(false);
+    toast.success("Catégorie mise à jour avec succès.");
+  };
+
+  const openEditModal = (category: Category) => {
+    setName(category.name);
+    setDescription(category.description || "");
+    setEditMode(true);
+    setEditingCategoryId(category.id);
+    (
+      document.getElementById("category_modal") as HTMLDialogElement
+    )?.showModal();
+  };
+
+  const handleDeleteCategory = async (categoryId: string) => {
+    const confirmDelete = confirm("Voulez-vous vraiment supprimer cette catégorie ? Tous les produits associés seront également supprimés")
+    if (!confirmDelete) return;
+    await deleteCategory(categoryId, email);
+    await loadCategories();
+    toast.success("Catégorie supprimée avec succès.")
   }
+
+
   return (
     <Wrapper>
       <div>
@@ -58,6 +100,42 @@ const page = () => {
             Ajouter une catégorie
           </button>
         </div>
+        {categories.length > 0 ? (
+          <div>
+            {categories.map((category) => (
+              <div
+                key={category.id}
+                className="mb-2 p-5 border-base-200 rounded-3xl flex justify-between items-center"
+              >
+                <div>
+                  <strong className="text-lg"> {category.name}</strong>
+                  <div className="text-sm"> {category.description}</div>
+                </div>
+
+                <div className="flex gap-2">
+                  <button
+                    className="btn btn-sm"
+                    onClick={() => openEditModal(category)}
+                  >
+                    <Pencil className="w-4 h-4" />
+                  </button>
+
+                  <button
+                    className="btn btn-sm btn-error"
+                    onClick={() => handleDeleteCategory(category.id)}
+                  >
+                    <Trash className="w-4 h-4" />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <EmptyState
+            message={"Aucun catégorie disponible"}
+            IconComponent="Group"
+          />
+        )}
       </div>
 
       <CategoryModal
@@ -70,7 +148,6 @@ const page = () => {
         onSubmit={editMode ? handleUpdateCategory : handleCreateCategory}
         editMode={editMode}
       />
-
     </Wrapper>
   );
 };
